2a412df0912da34cb39ef56fb59d487d1c771e98
chenhu2015 <chenhu2015@users.noreply.github.com>
2021-03-13 09:45:24 -0600

    update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,45 +1,64 @@
+drop table game_tag;
+drop table review;
 drop table game;
 drop table tag;
-drop table game_tag;
 drop table reviewer;
-drop table review;
+
 
 create table game (
-    name varchar(255) PRIMARY KEY,
-    picture bytea,
+    id SERIAL PRIMARY KEY,
+    name varchar(255),
+    picture varchar(255),
     video varchar(255),
     popularity int,
     rating float,
-    description varchar(255),
-    platform varchar(255)
+    shortdes text,
+    longdes text,
+    platform varchar(255),
+    review_number int
 );
 
 create table tag (
-    name varchar(255) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name varchar(255)
 );
 
 create table game_tag (
-    game varchar(255),
-    tag varchar(255),
+    game_id int,
+    tag_id int,
     count int,
-    CONSTRAINT fk_game FOREIGN KEY(game) REFERENCES game(name),
-    CONSTRAINT fk_tag FOREIGN KEY(tag) REFERENCES tag(name),
-    PRIMARY KEY(game, tag)
+    CONSTRAINT fk_game FOREIGN KEY(game_id) REFERENCES game(id),
+    CONSTRAINT fk_tag FOREIGN KEY(tag_id) REFERENCES tag(id),
+    PRIMARY KEY(game_id, tag_id)
 );
 
 create table reviewer (
-    id serial PRIMARY KEY,
-    oauth_id varchar(255)
+    id SERIAL PRIMARY KEY,
+    oauth_id varchar(255),
+    avatarlink varchar(255)
 );
 
 create table review (
-    id serial PRIMARY KEY,
-    reviewer int,
-    game varchar(255),
+    id SERIAL PRIMARY KEY,
+    reviewer_id int,
+    game_id int,
     timestamp TIMESTAMP DEFAULT NOW(),
     title varchar(255),
     content text,
     rating int,
-    CONSTRAINT fk_reviewer FOREIGN KEY(reviewer) REFERENCES reviewer(id),
-    CONSTRAINT fk_game FOREIGN KEY(game) REFERENCES game(name)
+    CONSTRAINT fk_reviewer FOREIGN KEY(reviewer_id) REFERENCES reviewer(id),
+    CONSTRAINT fk_game FOREIGN KEY(game_id) REFERENCES game(id)
+);
+
+create table picture(
+    id SERIAL PRIMARY KEY,
+    picturelink varchar(255)
+);
+
+create table game_picture (
+    game_id int,
+    picture_id int,
+    CONSTRAINT fk_game FOREIGN KEY(game_id) REFERENCES game(id),
+    CONSTRAINT fk_tag FOREIGN KEY(picture_id) REFERENCES picture(id),
+    PRIMARY KEY(game_id, picture_id)
 );
